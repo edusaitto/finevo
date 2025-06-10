a73832f488d9d47c4acbee7dac80cbd28630d22e
Eduardo Saito <61482919+edusaitto@users.noreply.github.com>
2025-06-10 02:18:25 +0000

    feat: create backend foundation

---
+++ infra/controller.js
@@ -0,0 +1,35 @@
+import {
+  InternalServerError,
+  MethodNotAllowedError,
+  ValidationError,
+  NotFoundError,
+} from "infra/errors";
+
+function onNoMatchHandler(_, res) {
+  const publicErrorObject = new MethodNotAllowedError();
+  res.status(publicErrorObject.statusCode).json(publicErrorObject);
+}
+
+function onErrorHandler(error, _, res) {
+  if (error instanceof ValidationError || error instanceof NotFoundError) {
+    return res.status(error.statusCode).json(error);
+  }
+
+  const publicErrorObject = new InternalServerError({
+    statusCode: error.statusCode,
+    cause: error,
+  });
+
+  console.error(publicErrorObject);
+
+  res.status(publicErrorObject.statusCode).json(publicErrorObject);
+}
+
+const controller = {
+  errorHandlers: {
+    onNoMatch: onNoMatchHandler,
+    onError: onErrorHandler,
+  },
+};
+
+export default controller;
---
+++ pages/index.js
@@ -0,0 +1,5 @@
+function Home() {
+  return <h1>Construindo algo incrível aqui!</h1>;
+}
+
+export default Home;
