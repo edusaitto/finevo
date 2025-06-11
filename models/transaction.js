e2afba6794004bf071fdfc09061b00ce4ede6b61
Eduardo Saito <61482919+edusaitto@users.noreply.github.com>
2025-06-11 13:57:17 +0000

    feat: improve `transaction` model queries

--- models/transaction.js
+++ models/transaction.js
@@ -5,6 +5,20 @@ async function create(transactionInputValues) {
   return newTransaction;
 
   async function runInsertQuery(transactionInputValues) {
+    const type = await database.query({
+      text: `
+        SELECT
+          *
+        FROM 
+          types
+        WHERE
+          title = $1
+        LIMIT 
+          1
+        ;`,
+      values: [transactionInputValues.type],
+    });
+
     const results = await database.query({
       text: `
         INSERT INTO 
@@ -18,7 +32,7 @@ async function create(transactionInputValues) {
         transactionInputValues.userId,
         transactionInputValues.title,
         transactionInputValues.value,
-        transactionInputValues.type,
+        type.rows[0].id,
         transactionInputValues.category,
         transactionInputValues.card,
         transactionInputValues.bill,
@@ -31,22 +45,34 @@ async function create(transactionInputValues) {
   }
 }
 
-async function findAllByUserId(userId) {
-  const transactions = (await runSelectQuery(userId)) ?? [];
-
+async function findAllByUserId(userId, type) {
+  const transactions = (await runSelectQuery(userId, type)) ?? [];
   return transactions;
 
-  async function runSelectQuery(userId) {
+  async function runSelectQuery(userId, type) {
+    let query = `
+      SELECT 
+        transactions.*, 
+        types.title AS type_title
+      FROM 
+        transactions
+      JOIN 
+        categories ON transactions.category = categories.id
+      JOIN 
+        types ON categories.type = types.id
+      WHERE 
+        transactions.user_id = $1
+    `;
+    const values = [userId];
+
+    if (type) {
+      query += ` AND type = $2`;
+      values.push(type);
+    }
+
     const results = await database.query({
-      text: `
-        SELECT
-          *
-        FROM
-          transactions
-        WHERE 
-          user_id = $1
-        ;`,
-      values: [userId],
+      text: query,
+      values,
     });
 
     return results.rows;
