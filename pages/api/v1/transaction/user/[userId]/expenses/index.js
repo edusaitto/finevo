cafca2e576f22a068d9d5c93ae84ce42cfd20dfa
Eduardo Saito <61482919+edusaitto@users.noreply.github.com>
2025-06-11 13:57:44 +0000

    feat: add new `transaction` routes per type

---
+++ pages/api/v1/transaction/user/[userId]/expenses/index.js
@@ -0,0 +1,15 @@
+import { createRouter } from "next-connect";
+import controller from "infra/controller";
+import transaction from "models/transaction.js";
+
+const router = createRouter();
+
+router.get(getHandler);
+
+export default router.handler(controller.errorHandlers);
+
+async function getHandler(request, response) {
+  const { userId } = request.query;
+  const transactions = await transaction.findAllByUserId(userId, "expense");
+  return response.status(200).json(transactions);
+}
