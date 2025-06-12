54dc827b4cf899f67e7f8024efed4b753f617666
Eduardo Saito <61482919+edusaitto@users.noreply.github.com>
2025-06-12 17:16:27 +0000

    feat: add `pwa` stuff

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,16 @@
+import Head from "next/head";
 import "styles/globals.css";
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#00acc1" />
+        <link rel="apple-touch-icon" href="/icons/favicon-192x192.png" />
+        <meta name="description" content="Versão PWA do App Finevo" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
