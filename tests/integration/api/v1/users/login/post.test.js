2341cf91b379d04bf0a904992ac4f9b7611db69a
Eduardo Saito <61482919+edusaitto@users.noreply.github.com>
2025-06-14 16:05:36 +0000

    refactor: improve `login` tests

--- tests/integration/api/v1/users/login/post.test.js
+++ tests/integration/api/v1/users/login/post.test.js
@@ -8,43 +8,89 @@ beforeAll(async () => {
 
 describe("POST /api/v1/users/login", () => {
   describe("Anonymous user", () => {
-    test("With unique and valid data", async () => {
-      const response = await fetch("http://localhost:3000/api/v1/users", {
+    test("With valid user data", async () => {
+      await orchestrator.createUser({
+        email: "login@teste.com",
+        password: "login123",
+      });
+
+      const response = await fetch("http://localhost:3000/api/v1/users/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
         body: JSON.stringify({
-          name: "saito",
-          email: "saito@gmail.com",
-          password: "senha123",
+          email: "login@teste.com",
+          password: "login123",
         }),
       });
 
       expect(response.status).toBe(201);
 
-      const responseLogin = await fetch(
-        "http://localhost:3000/api/v1/users/login",
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            email: "saito@gmail.com",
-            password: "senha123",
-          }),
+      const responseBody = await response.json();
+
+      expect(responseBody.token).toBeDefined();
+      expect(responseBody.token.length).toBeGreaterThan(0);
+      expect(responseBody.userId).toBeDefined();
+      expect(responseBody.userId.length).toBeGreaterThan(0);
+    });
+
+    test("With incorrect user 'email'", async () => {
+      await orchestrator.createUser({
+        email: "login@email.com",
+        password: "login123",
+      });
+
+      const response = await fetch("http://localhost:3000/api/v1/users/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
         },
-      );
+        body: JSON.stringify({
+          email: "email@login.com",
+          password: "login123",
+        }),
+      });
 
-      expect(responseLogin.status).toBe(201);
+      expect(response.status).toBe(404);
 
-      const responseLoginBody = await responseLogin.json();
+      const responseBody = await response.json();
 
-      expect(responseLoginBody.token).toBeDefined();
-      expect(responseLoginBody.token.length).toBeGreaterThan(0);
-      expect(responseLoginBody.userId).toBeDefined();
-      expect(responseLoginBody.userId.length).toBeGreaterThan(0);
+      expect(responseBody).toEqual({
+        name: "NotFoundError",
+        message: "E-mail e/ou senha incorretos.",
+        action: "Verifique se os dados foram digitados corretamente.",
+        status_code: 404,
+      });
+    });
+
+    test("With incorrect user 'password'", async () => {
+      await orchestrator.createUser({
+        email: "login@password.com",
+        password: "login123",
+      });
+
+      const response = await fetch("http://localhost:3000/api/v1/users/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email: "login@password.com",
+          password: "123login",
+        }),
+      });
+
+      expect(response.status).toBe(404);
+
+      const responseBody = await response.json();
+
+      expect(responseBody).toEqual({
+        name: "NotFoundError",
+        message: "E-mail e/ou senha incorretos.",
+        action: "Verifique se os dados foram digitados corretamente.",
+        status_code: 404,
+      });
     });
   });
 });
