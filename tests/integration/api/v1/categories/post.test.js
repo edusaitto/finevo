df3c59fceb69811ff7dddf0390be11f8bd172a04
Eduardo Saito <61482919+edusaitto@users.noreply.github.com>
2025-06-15 22:34:24 +0000

    chore: change `categories` test description and var names

--- tests/integration/api/v1/categories/post.test.js
+++ tests/integration/api/v1/categories/post.test.js
@@ -7,36 +7,33 @@ beforeAll(async () => {
 });
 
 describe("POST /api/v1/categories", () => {
-  describe("Anonymous user", () => {
-    test("With valid data", async () => {
+  describe("Logged user", () => {
+    test("Insert category with valid data", async () => {
       const user = await orchestrator.createUser();
 
-      const responseCategory = await fetch(
-        "http://localhost:3000/api/v1/categories",
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            userId: user.id,
-            title: "newCategory",
-          }),
+      const response = await fetch("http://localhost:3000/api/v1/categories", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
         },
-      );
+        body: JSON.stringify({
+          userId: user.id,
+          title: "newCategory",
+        }),
+      });
 
-      expect(responseCategory.status).toBe(201);
+      expect(response.status).toBe(201);
 
-      const responseCategoryBody = await responseCategory.json();
+      const responseBody = await response.json();
 
-      expect(responseCategoryBody.id).toBeDefined();
-      expect(responseCategoryBody.id).not.toBeNull();
+      expect(responseBody.id).toBeDefined();
+      expect(responseBody.id).not.toBeNull();
 
-      expect(responseCategoryBody.user_id).toBeDefined();
-      expect(responseCategoryBody.user_id).not.toBeNull();
+      expect(responseBody.user_id).toBeDefined();
+      expect(responseBody.user_id).not.toBeNull();
 
-      expect(responseCategoryBody.title).toBeDefined();
-      expect(responseCategoryBody.title).not.toBeNull();
+      expect(responseBody.title).toBeDefined();
+      expect(responseBody.title).not.toBeNull();
     });
   });
 });
